4e718bb73c4e277dcaa6fe336f97820e3cdebd5d
mondain-dev <mondain.sciamhaevensis@gmail.com>
2018-12-18 16:08:19 +1100

    Create ChnHistPhon_2_run_softImpute.R

---
+++ ChnHistPhon_2_run_softImpute.R
@@ -0,0 +1,23 @@
+library(softImpute)
+DIR_RESULTS = '/path/to/ChnHistPhon/results'
+
+# load results
+characters = read.csv(paste(DIR_RESULTS, 'ChnChar.csv', sep="/"))
+X  <- read.csv(paste(DIR_RESULTS, 'ChnCharData.csv', sep="/"))
+Xc <- as(data.matrix(X), "Incomplete")
+
+# run SVD softImpute
+X_SVD <- softImpute(Xc, 200, type='svd', trace.it=TRUE, maxit = 200)
+
+
+for (i in seq(5))
+{
+  loading_feature = X_SVD$v[order(abs(X_SVD$v[,i]), decreasing = T)]
+  names(loading_feature)  = names(X)[order(abs(X_SVD$v[,i]), decreasing = T)]
+
+  loading_characters = X_SVD$u[order(abs(X_SVD$u[,i]), decreasing = T)]
+  names(loading_characters)  = characters$character[order(abs(X_SVD$u[,i]), decreasing = T)]
+
+  print(loading_feature[1:10])
+  print(loading_characters[1:100])
+}
