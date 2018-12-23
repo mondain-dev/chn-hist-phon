c8ede90a7cbc725cc159279f03c491c78564921b
mondain-dev <mondain.sciamhaevensis@gmail.com>
2018-12-23 23:09:00 +1100

    Update ChnHistPhon_2_run_SoftImpute_DictionaryLearning.py

--- ChnHistPhon_2_run_SoftImpute_DictionaryLearning.py
+++ ChnHistPhon_2_run_SoftImpute_DictionaryLearning.py
@@ -6,17 +6,21 @@ import numpy as np
 import scipy
 
 from fancyimpute import SoftImpute
-from sklearn.decomposition import DictionaryLearning, SparseCoder
+from sklearn.decomposition import TruncatedSVD, DictionaryLearning, SparseCoder
+
+from sqlalchemy import create_engine
 
 # configuration
 DIR_DATA= '/path/to/ChnHistPhon/results'
+con = create_engine('postgresql://@localhost:5432/chp_db')
 
 # parameters
 n_rank = 200
-n_components = 300
+n_entries = 200
 
 # Loading Data
-X = np.genfromtxt(os.path.join(DIR_DATA, 'ChnCharData.csv'), delimiter=',', skip_header=True)
+ChnCharData = pd.read_csv(os.path.join(DIR_DATA, 'ChnCharData.csv'))
+X = ChnCharData.values
 with open(os.path.join(DIR_DATA, 'ChnCharData.csv'), 'r') as f:
     csv_reader = csv.reader(f)
     features = [s.decode('utf-8') for s in next(csv_reader)]
@@ -26,27 +30,38 @@ char_list = pd.read_csv(os.path.join(DIR_DATA, 'ChnChar.csv'))
 # run soft-impute
 idx_nan  = np.where(np.isnan(X))
 idx_mask = np.where(~np.isnan(X))
-Z = SoftImpute().fit_transform(X)
+Z = SoftImpute(max_rank = n_rank, max_iters = 250).fit_transform(X)
 
-# run sparse dictionary learning 
-dictionary_learning = DictionaryLearning(n_components=n_components, max_iter=100, verbose=2)
-X_fit = dictionary_learning.fit(Z)
-dictionary = X_fit.components_
-sparse_coder = SparseCoder(dictionary)
-code = sparse_coder.transform(Z)
+svd = TruncatedSVD(n_rank) 
+svd.fit(Z)
+X_imputed = svd.transform(Z).dot(svd.components_)
+e = ((X_imputed[idx_mask] - X[idx_mask]) ** 2).sum()/(X[idx_mask] ** 2).sum()
+print("Imputation error: %.2f%%" % (e*100))
+
+# run sparse dictionary learning
+dictionary_learning = DictionaryLearning(n_components = n_entries, max_iter = 150, transform_algorithm = "lasso_lars", positive_code=True, verbose = 2)
+dictionary_learning.fit(Z)
+dictionary = dictionary_learning.components_
+code = dictionary_learning.transform(Z)
 
 X_recon = code.dot(dictionary)
 e = ((X_recon[idx_mask] - X[idx_mask]) ** 2).sum()/(X[idx_mask] ** 2).sum()
 print("Reconstruction error: %.2f%%" % (e*100))
 
-with open(os.path.join(DIR_DATA, 'dictionary.csv'), 'w') as f:
-    csv_writer = csv.writer(f, delimiter=',')
-    csv_writer.writerow([f.encode('utf-8') for f in features])
-    for r in dictionary:
-        csv_writer.writerow(r)
+char_code = pd.DataFrame(code, columns = ['entry_' + str(i+1) for i in range(code.shape[1])])
+char_code = pd.concat([pd.read_csv(os.path.join(DIR_DATA, 'ChnChar.csv')), char_code], axis=1)
 
-with open(os.path.join(DIR_DATA, 'code.csv'), 'w') as f:
-    csv_writer = csv.writer(f, delimiter=',')
-    csv_writer.writerow(['entry_%d' % entry for entry in range(n_components)])
-    for r in code:
-        csv_writer.writerow(r)
+dictionary = pd.DataFrame(dictionary_learning.components_, columns = ['feature_' + str(i+1) for i in range(X.shape[1])])
+dictionary['entry'] = ['entry_' + str(i+1) for i in range(dictionary.shape[0])]
+
+feature_name = pd.DataFrame.from_dict({'feature_id': ['feature_' + str(i+1) for i in range(ChnCharData.shape[1])], 'feature': ChnCharData.columns.tolist()})
+
+# write to database
+char_code.to_sql('char_code', con, if_exists='replace', index=False)
+dictionary.to_sql('dictionary', con, if_exists='replace', index=False)
+feature_name.to_sql('feature_name', con, if_exists='replace', index=False)
+
+# write to csv
+char_code.to_csv(os.path.join(DIR_DATA, 'char_code.csv'), index=False)
+dictionary.to_csv(os.path.join(DIR_DATA, 'dictionary.csv'), index=False)
+feature_name.to_csv(os.path.join(DIR_DATA, 'feature_name.csv'), index=False)
